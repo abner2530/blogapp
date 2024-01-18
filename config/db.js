25b9caf4d32214c737628528b4aa6db920ba730e
abner monteiro <abner_monteiro_@hotmail.com>
2024-01-18 12:19:13 -0300

    att

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production") {
-     module.exports = {mongoURI: "mongodb+srv://admin:<KRsPzwvkO4vUYkBU>@abrigo.xoyo2kb.mongodb.net/?retryWrites=true&w=majority"}
+     module.exports = {mongoURI: "mongodb+srv://mongodb:123@blogapp.80rp2as.mongodb.net/?retryWrites=true&w=majority"}
 } else {
     module.exports = {mongoURI: "mongodb://127.0.0.1:27017/blogapp"}
 }
